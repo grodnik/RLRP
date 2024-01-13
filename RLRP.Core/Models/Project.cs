173a86281719fd41becdb5b23fe727e822914936
grodnik <jbarnes@dbasystems.com>
2024-01-13 13:00:20 -0600

    Project Model and InMemory Project Repository Built

---
+++ RLRP.Core/Models/Project.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLRP.Core.Models
+{
+  public class Project
+  {
+
+    public string Id { get; set; }
+
+    [StringLength(40)]
+    [DisplayName("Project Title")]
+    public string Title { get; set; }
+    public string Area { get; set; }
+    public string Status { get; set; }
+    public string OldNbr { get; set; }
+    public DateTime DefinedDate { get; set; }
+    public decimal BudgetYear { get; set; }
+    public decimal BudgetCost { get; set; }
+    public decimal BudgetLife { get; set; }
+    public string OddEven { get; set; }
+    public string Notes { get; set; }
+
+    public Project()
+    {
+      this.Id = Guid.NewGuid().ToString();
+    }
+
+  }
+}
---
+++ RLRP.DataAccess.InMemory/ProjectRepository.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using RLRP.Core.Models;
+
+namespace RLRP.DataAccess.InMemory
+{ 
+  public class ProjectRepository
+  {
+    ObjectCache cache = MemoryCache.Default;
+    List<Project> projects;
+
+    public ProjectRepository()
+    {
+      projects = cache["projects"] as List<Project>;
+      if (projects == null)
+      {
+        projects = new List<Project>();
+      }
+    }
+
+    public void Commit()
+    {
+      cache["projects"] = projects;
+    }
+
+    public void Insert(Project p)
+    {
+      projects.Add(p);
+    }
+
+    public void Update(Project project)
+    {
+      Project projectToUpdate = projects.Find(p => p.Id == project.Id);
+
+      if (projectToUpdate != null)
+      {
+        projectToUpdate = project;
+      }
+      else
+      {
+        throw new Exception("Project Not Found");
+      }
+    }
+
+    public Project Find(string Id)
+    {
+      Project project = projects.Find(p => p.Id == Id);
+
+      if (project != null)
+      {
+        return project;
+      }
+      else
+      {
+        throw new Exception("Project Not Found");
+      }
+    }
+
+    public IQueryable<Project> Collection()
+    {
+      return projects.AsQueryable();
+    }
+
+    public void Delete(string Id)
+    {
+      Project projectToDelete = projects.Find(p => p.Id == Id);
+
+      if (projectToDelete != null)
+      {
+        projects.Remove(projectToDelete);
+      }
+      else
+      {
+        throw new Exception("Project Not Found");
+      }
+    }
+
+  }
+}
